900caef22ce726e38ffebe1beacaf3a73f4100b9
Sejoon Oh <sejun6431@gmail.com>
2024-07-31 22:43:03 -0700

    Update ATR-2FT.sh

--- ATR-2FT.sh
+++ ATR-2FT.sh
@@ -1,6 +1,6 @@
 python src/ATR-2FT.py \
   --bert_model opt-350m \
-  --mf_model_path  src/saved/UniSRec-Jun-27-2023_02-29-15.pth\
+  --mf_model_path  src/saved/UniSRec_Fine_Tuned_on_Amazon_Book.pth\
   --output result/amazon/2FT/ranking \
 	--epochs 2 \
 	--train_batch_size 16\
